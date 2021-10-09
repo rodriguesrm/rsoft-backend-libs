5fa2140e1699874cd92d96da32fd16358910ccb7
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2021-10-09 08:44:39 -0300

    Added Lib.Contacts project

---
+++ src/RSoft.Lib.Contracts/Events/UserChangedEvent.cs
@@ -0,0 +1,80 @@
+﻿using RSoft.Lib.Common.Enums;
+using RSoft.Lib.Messaging.Contracts;
+using System;
+
+namespace RSoft.Lib.Contracts.Events
+{
+
+    /// <summary>
+    /// Occurs when an user is changed/udpated.
+    /// </summary>
+    public class UserChangedEvent : IMessageEvent
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a new event instance
+        /// </summary>
+        /// <param name="id">User id key value</param>
+        /// <param name="firstName">User first name</param>
+        /// <param name="lastName">User last name</param>
+        /// <param name="email">User e-mail address</param>
+        /// <param name="bornDate">User date of birth</param>
+        /// <param name="type">User type</param>
+        /// <param name="isActive">Active status flag</param>
+        public UserChangedEvent(Guid id, string firstName, string lastName, string email, DateTime? bornDate, UserType type, bool isActive)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BornDate = bornDate;
+            Type = type;
+            IsActive = isActive;
+        }
+        
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// User id key value
+        /// </summary>
+        public Guid Id { get; private set; }
+
+        /// <summary>
+        /// User first name
+        /// </summary>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// User last name
+        /// </summary>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// User e-mail
+        /// </summary>
+        public string Email { get; private set; }
+
+        /// <summary>
+        /// User's date of birth
+        /// </summary>
+        public DateTime? BornDate { get; private set; }
+
+        /// <summary>
+        /// User type
+        /// </summary>
+        public UserType Type { get; private set; }
+
+        /// <summary>
+        /// Active status flag
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ src/RSoft.Lib.Contracts/Events/UserDeletedEvent.cs
@@ -0,0 +1,37 @@
+﻿using RSoft.Lib.Messaging.Contracts;
+using System;
+
+namespace RSoft.Lib.Contracts.Events
+{
+
+    /// <summary>
+    /// Occurs when an user is removed/deleted.
+    /// </summary>
+    public class UserDeletedEvent : IMessageEvent
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a new event instance
+        /// </summary>
+        /// <param name="id">User id key value</param>
+        public UserDeletedEvent(Guid id)
+        {
+            Id = id;
+        }
+        
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// User id key value
+        /// </summary>
+        public Guid Id { get; private set; }
+
+        #endregion
+
+    }
+}
