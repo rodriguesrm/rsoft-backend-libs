1c177c735996d6b33179f754736adb719fdfe703
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2023-01-26 11:23:56 -0300

    Added messageId in LogExtension for Log Message

--- src/RSoft.Lib.Messaging/Extensions/LogExtension.cs
+++ src/RSoft.Lib.Messaging/Extensions/LogExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 
 namespace RSoft.Lib.Messaging.Extensions
@@ -15,13 +16,15 @@ namespace RSoft.Lib.Messaging.Extensions
         /// </summary>
         /// <param name="logger">Microsoft.Extensions.Logging.ILogger to write to</param>
         /// <param name="message">Log text message</param>
-        /// <param name="content">Adicional content to log</param>
+        /// <param name="messageId">The messageId assigned to the message when it was initially Sent</param>
+        /// <param name="messageContent">Adicional content to log</param>
         /// <param name="contentName">Tag content name to log</param>
-        public static void LogInformation(this ILogger logger, string message, string content, string contentName = "MessageContent")
+        public static void LogInformation(this ILogger logger, string message, Guid? messageId, string messageContent, string contentName = "MessageContent")
         {
             IList<KeyValuePair<string, object>> pairs = new List<KeyValuePair<string, object>>
             {
-                new KeyValuePair<string, object>(contentName, content)
+                new KeyValuePair<string, object>("MessageId", messageId),
+                new KeyValuePair<string, object>(contentName, messageContent)
             };
             logger.Log(LogLevel.Information, new EventId(1010, "RSoft:Process:Message"), state: pairs, null, (i, e) => { return message; });
         }
