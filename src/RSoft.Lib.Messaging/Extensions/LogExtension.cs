290610f7c4e9af47686a7b0a9e393a8051a35872
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2023-01-26 11:37:13 -0300

    Added new LogExtension method for messaging

--- src/RSoft.Lib.Messaging/Extensions/LogExtension.cs
+++ src/RSoft.Lib.Messaging/Extensions/LogExtension.cs
@@ -29,6 +29,21 @@ namespace RSoft.Lib.Messaging.Extensions
             logger.Log(LogLevel.Information, new EventId(1010, "RSoft:Process:Message"), state: pairs, null, (i, e) => { return message; });
         }
 
+        /// <summary>
+        /// Formats and writes an informational log message.
+        /// </summary>
+        /// <param name="logger">Microsoft.Extensions.Logging.ILogger to write to</param>
+        /// <param name="message">Log text message</param>
+        /// <param name="messageId">The messageId assigned to the message when it was initially Sent</param>
+        public static void LogInformation(this ILogger logger, string message, Guid? messageId)
+        {
+            IList<KeyValuePair<string, object>> pairs = new List<KeyValuePair<string, object>>
+            {
+                new KeyValuePair<string, object>("MessageId", messageId),
+            };
+            logger.Log(LogLevel.Information, new EventId(1010, "RSoft:Process:Message"), state: pairs, null, (i, e) => { return message; });
+        }
+
     }
 
 }
